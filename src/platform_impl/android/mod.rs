629c1436248293155bbfbfad13f333ba836efb85
Matt Campbell <mattcampbell@pobox.com>
2024-11-25 14:23:15 -0600

    Show and hide the soft input the normal way, via android-activity, now that we're using GameActivity

--- src/platform_impl/android/mod.rs
+++ src/platform_impl/android/mod.rs
@@ -10,7 +10,6 @@ use android_activity::input::{InputEvent, KeyAction, Keycode, MotionAction, Moti
 use android_activity::{
     AndroidApp, AndroidAppWaker, ConfigurationRef, InputStatus, MainEvent, Rect,
 };
-use jni::{objects::JObject, JavaVM};
 use tracing::{debug, trace, warn};
 
 use crate::cursor::Cursor;
@@ -823,26 +822,12 @@ impl DeviceId {
 #[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
 pub struct PlatformSpecificWindowAttributes;
 
-fn show_hide_keyboard_fallible(app: AndroidApp, show: bool) -> Result<(), jni::errors::Error> {
-    // After Android R, it is no longer possible to show the soft keyboard
-    // with `showSoftInput` alone.
-    // Here we use `WindowInsetsController`, which is the other way.
-    let vm = unsafe { JavaVM::from_raw(app.vm_as_ptr() as _)? };
-    let activity = unsafe { JObject::from_raw(app.activity_as_ptr() as _) };
-    let mut env = vm.attach_current_thread()?;
-    let window = env.call_method(&activity, "getWindow", "()Landroid/view/Window;", &[])?.l()?;
-    let wic = env
-        .call_method(window, "getInsetsController", "()Landroid/view/WindowInsetsController;", &[])?
-        .l()?;
-    let window_insets_types = env.find_class("android/view/WindowInsets$Type")?;
-    let ime_type = env.call_static_method(&window_insets_types, "ime", "()I", &[])?.i()?;
-    env.call_method(&wic, if show { "show" } else { "hide" }, "(I)V", &[ime_type.into()])?.v()
-}
-
 fn show_hide_keyboard(app: AndroidApp, show: bool) {
-    if let Err(e) = show_hide_keyboard_fallible(app, show) {
-        tracing::error!("Showing or hiding the soft keyboard failed: {e:?}");
-    };
+    if show {
+        app.show_soft_input(false);
+    } else {
+        app.hide_soft_input(false);
+    }
 }
 
 pub(crate) struct Window {
